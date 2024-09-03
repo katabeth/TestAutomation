b9a77423a6e8e47a840fc6adb74367160acbbc06
Katherine <katabeth.ch@gmail.com>
2024-09-03 13:40:38 +0100

    Searching for specific elements in a return body

---
+++ APITesting/src/test/java/com/sparta/kch/restassured/PrimaryCareTrust.java
@@ -0,0 +1,14 @@
+package com.sparta.kch.restassured;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PrimaryCareTrust {
+
+    @Test
+    @DisplayName("Search for Primary Care Trust")
+    public void testPrimaryCareTrust() {
+
+    }
+
+}
--- APITesting/src/test/java/com/sparta/kch/restassured/SinglePostcodeTest.java
+++ APITesting/src/test/java/com/sparta/kch/restassured/SinglePostcodeTest.java
@@ -1,23 +1,86 @@
 package com.sparta.kch.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 public class SinglePostcodeTest {
+
+    private static Response response;
+
+    @BeforeAll
+    public static void setup() {
+        response = RestAssured
+                .given()
+                .baseUri("https://api.postcodes.io")
+                .basePath("/postcodes")
+                .header("Accept", "text/json")
+                .when()
+                .get("/EC2Y5AS")
+                .thenReturn()
+        ;
+    }
 
     @Test
     @DisplayName("Status code 200 returned")
-    public void testStatusCode() {
+    public void testStatusCode200() {
         RestAssured
                 .given()
-                    .baseUri("https://api.postcodes.io")
-                    .basePath("/postcodes")
+                .baseUri("https://api.postcodes.io")
+                .basePath("/postcodes")
+                .header("Accept", "text/json")
+                .log().all()
                 .when()
-                    .get("/EC2Y5AS")
+                .get("/EC2Y5AS")
                 .then()
-                    .assertThat()
-                    .statusCode(200)
-                ;
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    @DisplayName("Status code 200 returned v2")
+    public void testStatusCode200_v2() {
+        RestAssured
+                .given()
+                .baseUri("https://api.postcodes.io/postcodes/EC2Y5AS")
+                .when()
+                .get()
+                .then()
+                .assertThat()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    @DisplayName("Status code 200 returned v3")
+    public void testStatusCode200_v3() {
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.is(200));
+    }
+
+    @Test
+    @DisplayName("Server name is in Header")
+    public void testServerName() {
+        MatcherAssert.assertThat(response.getHeader("Server"), Matchers.is("cloudflare"));
+    }
+
+    @Test
+    @DisplayName("Primary Care Trust returned in response")
+    public void testPrimaryCareTrust() {
+        MatcherAssert.assertThat(response.getBody().jsonPath().getString("result.primary_care_trust"), Matchers.is("City and Hackney Teaching"));
+    }
+
+    @Test
+    @DisplayName("Number of codes returned is 14")
+    public void testNumberOfCodesReturned() {
+        MatcherAssert.assertThat(response.getBody().jsonPath().getMap("result.codes").size(), Matchers.is(14));
     }
 }
