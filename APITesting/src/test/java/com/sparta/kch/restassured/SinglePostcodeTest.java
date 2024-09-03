3d0b748dd0cc874df100b9379d471cfde8425cad
Katherine <katabeth.ch@gmail.com>
2024-09-03 11:31:11 +0100

    RESTAssured first test made, run and passed

--- APITesting/src/test/java/com/sparta/kch/restassured/SinglePostcodeTest.java
+++ APITesting/src/test/java/com/sparta/kch/restassured/SinglePostcodeTest.java
@@ -1,5 +1,23 @@
 package com.sparta.kch.restassured;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class SinglePostcodeTest {
-    
+
+    @Test
+    @DisplayName("Status code 200 returned")
+    public void testStatusCode() {
+        RestAssured
+                .given()
+                    .baseUri("https://api.postcodes.io")
+                    .basePath("/postcodes")
+                .when()
+                    .get("/EC2Y5AS")
+                .then()
+                    .assertThat()
+                    .statusCode(200)
+                ;
+    }
 }
